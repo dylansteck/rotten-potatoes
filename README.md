9d0715b177faf9d5b91ecbb0ee279c9eb5ea2c3a
Dylan <dylancsteck@gmail.com>
2018-01-18 17:16:00 +0000

    updated app.js and readme.md

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 ### @dylansteck
 
 Test app from Make School Online Academy with Node.js
+
+
+# To run in Cloud9:
+
+    - Add `"start": "nodemon app.js"` to the scripts section of `package.json`
+    - Run the app on port 8080
+    - Preview on https://workspacename-username.c9users.io
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req,res) => {
-    res.send('Hello World!')
+    res.render('home', {msg: 'Hello World!'});
 })
 
 app.listen(8080, () =>{
